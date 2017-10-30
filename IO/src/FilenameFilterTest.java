5f6187e7f8df156bdeb50b51dc31b4c2540fb464
challengerzsz <challengerzsz@126.com>
2017-10-30 21:26:14 +0800

    文件过滤器实现FilenameFilter接口

---
+++ IO/src/FilenameFilterTest.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class FilenameFilterTest {
+    public static void main(String[] args) {
+        File file = new File(".");
+        //使用Lambda表达式(目标类型为FilenameFilter)实现文件过滤器
+        //如果文件名以.java结尾，或者文件对应一个路径，则返回true
+        String [] nameList = file.list((dir, name) ->
+                name.endsWith(".java") || new File(name).isDirectory());
+        //下面将输出.idea out src
+
+        for (String name : nameList) {
+            System.out.println(name);
+        }
+    }
+
+}
