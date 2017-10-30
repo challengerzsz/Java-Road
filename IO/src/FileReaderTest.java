b5f61f1fb4a9944d68c72aaedd5af08acf0f4770
challengerzsz <challengerzsz@126.com>
2017-10-30 22:49:38 +0800

    使用字符输入流并且使用try catch 自动关闭资源

--- IO/src/FileReaderTest.java
+++ IO/src/FileReaderTest.java
@@ -2,11 +2,11 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReaderTest {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (
                 //声明并且初始化一个可自动关闭的资源
                 //创建字符输入流
-             FileReader fr = new FileReader("e:\\Java-Road\\IO\\src\\FileInputStreamTest.java")){
+             FileReader fr = new FileReader("e:\\Java-Road\\IO\\src\\FileInputStreamTest.java")) {
             //创建一个长度为32的“竹筒”
             char[] cbuf = new char[32];
             //用于保存实际读取的字节数
