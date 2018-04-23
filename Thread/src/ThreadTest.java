2c782c59a2d98d642fc90806cf3aee5c175c4adf
challengerzsz <challengerzsz@126.com>
2018-04-23 23:53:52 +0800

    线程安全测试

---
+++ Thread/src/ThreadTest.java
@@ -0,0 +1,25 @@
+public class ThreadTest {
+    public volatile int number = 0;
+
+
+    public void increase() {
+        number++;
+    }
+
+    public static void main(String[] args) {
+        final ThreadTest test = new ThreadTest();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) test.increase();
+            }).start();
+        }
+        //此方法返回活动线程的当前线程的线程组中的数量
+        //当活跃线程数>2时
+        //main线程yield等待等待上面十个线程执行完毕
+        while (Thread.activeCount() > 2) Thread.yield();
+
+        System.out.println(test.number);
+    }
+
+    
+}
